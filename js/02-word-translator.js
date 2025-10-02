aed73442d6e80f203f7cfd536d005ef48a10649e
priyarakkan <124619499+priyarakkan@users.noreply.github.com>
2025-10-01 17:38:19 -0700

    js

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,10 +1,13 @@
 let language = prompt('Enter the language code ("es", "de", "en", and "fr"): ');
-document.write(`Larger or Smaller <br>`);
-if(num1>num2) {
-    document.write(`Large number: ${num1}` );
-} else if(num1<num2) {
-    document.write(`Larger number : ${num2}`);
+document.write(`The Word Translator<br>`);
+if(language = 'es') {
+    document.write(`Hello World translated in Spanish is : Hola Mundo` );
+} else if(language = 'de') {
+    document.write(`Hello World translated in German is :Hallo Welt`);
 }
-else {
-    document.write('Both are equal!')
+else if(language = 'en') {
+    document.write(`Hello World`);
+}
+else if(language = 'fr') {
+    document.write(`Hello World translated in French is :Bonjour le monde`);
 }
